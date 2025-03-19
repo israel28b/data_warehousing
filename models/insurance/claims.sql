26f1a62f863a42e4dbd9f93bc7d9be91ff6e82b2
Israel Bell <a02372254@usu.edu>
2025-03-19 16:49:51 +0000

    Last update

--- models/insurance/claims.sql
+++ models/insurance/claims.sql
@@ -1,13 +1,29 @@
-SELECT 
-    fc.claim_id,
-    fc.policy_id,
-    dp.policy_type,
-    dc.customer_name,
-    da.agent_name,
-    fc.claim_amount,
-    dd.claim_date
-FROM {{ ref('fact_claim') }} fc
-LEFT JOIN {{ ref('dim_policy') }} dp ON fc.policy_id = dp.policy_id
-LEFT JOIN {{ ref('dim_customer') }} dc ON fc.customer_id = dc.customer_id
-LEFT JOIN {{ ref('dim_agent') }} da ON dp.agent_id = da.agent_id
-LEFT JOIN {{ ref('dim_date') }} dd ON fc.claim_date_id = dd.date_id;
+
+  {{ config(
+    materialized = 'table',
+    schema = 'dw_insurance'
+    )
+}}
+
+
+SELECT
+c.firstname as customer_first_name,
+c.lastname as customer_last_name,
+d.date_day,
+p.policyid,
+a.firstname as agent_first_name,
+a.lastname as agent_last_name,
+f.claimamount
+FROM {{ ref('fact_claim') }} f
+
+LEFT JOIN {{ ref('dim_customer') }} c
+    ON f.customer_key = c.customer_key
+
+LEFT JOIN {{ ref('dim_agent') }} a
+    ON f.agent_key = a.agent_key
+
+LEFT JOIN {{ ref('dim_policy') }} p
+    ON f.policy_key = p.policy_key
+
+LEFT JOIN {{ ref('dim_date') }} d
+    ON f.date_key = d.date_key
